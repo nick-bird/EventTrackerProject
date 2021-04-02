40153d16322334af00f35056f933ad341011545b
Nick Bird <bird.nick.95@gmail.com>
2021-04-02 15:05:53 -0600

    Stub Spring Boot project, connect to JPA project

---
+++ PracticeLog/src/test/java/com/skilldistillery/practicelog/PracticeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.practicelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
