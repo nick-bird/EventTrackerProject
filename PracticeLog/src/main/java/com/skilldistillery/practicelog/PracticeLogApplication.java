26ccf15c709bca2b967deb3e1109d56e0b18a3af
Nick Bird <bird.nick.95@gmail.com>
2021-04-05 08:53:42 -0600

    Configured for AWS deployment

--- PracticeLog/src/main/java/com/skilldistillery/practicelog/PracticeLogApplication.java
+++ PracticeLog/src/main/java/com/skilldistillery/practicelog/PracticeLogApplication.java
@@ -2,9 +2,17 @@ package com.skilldistillery.practicelog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.skilldistillery.practicelog.entities.PracticeLog;
 
 @SpringBootApplication
-public class PracticeLogApplication {
+public class PracticeLogApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(PracticeLog.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeLogApplication.class, args);
--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 # Overview
 
+
+
 ## How to Run
 
 ## REST API
 | HTTP Method | Resource URI | Request Body | Returns |
 |-------------|--------------|--------------|---------|
-| GET         | `api/lemurs` |              | List&lt;Lemur&gt;|
+| GET         | `api/logs` |                | List<PracticeLog>;|
+| GET         | 'api/logs/{logId}'|         | PracticeLog |
+| POST        | 'api/logs'   | {PracticeLog}| PracticeLog |
+| PUT         | 'api/logs/{logId}' | {PracticeLog} | PracticeLog |
+| DELETE      | 'api/logs/{logId}' |        | 204 or 404 status code |
